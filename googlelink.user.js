706aa5822f029afd7872ea39410ec2fbc0cab996
evecalm.neu <evecalm@live.com>
2013-05-03 14:44:27 +0800

    update googlelink.user.js to support google cse

--- googlelink.user.js
+++ googlelink.user.js
@@ -8,7 +8,7 @@
 // @match          https://www.google.com.hk/*
 // @updateURL      http://app.evecalm.com/search/googlelink.meta.js
 // @downloadURL    http://app.evecalm.com/search/googlelink.user.js
-// @version        0.51
+// @version        0.52
 // ==/UserScript==
 function proxy(fn) {
 	var script = document.createElement('script');
@@ -16,18 +16,31 @@ function proxy(fn) {
 	document.body.appendChild(script);
 }
 function main(Global){
-	var anchors = document.querySelectorAll('#ires h3 a'),
-		caches = document.querySelectorAll('#ires .action-menu-panel ul a'),
-		i,
-		j,
-		nullFun = function  () {
-			return true;
-		};
-	for (i = 0,j = anchors.length; i < j; ++i) {
-		anchors[i].onmousedown = nullFun;
-		if (caches[i]) {
-			caches[i].href = caches[i].href.replace('http://','https://');
-			caches[i].onmousedown = nullFun;
+	var csebody = document.querySelector('#cse-body'),
+		i = 0,
+		j = 0,
+		anchors = null,
+		caches = null;
+	if (csebody) {
+		i = 0;
+		csebody.addEventListener('DOMSubtreeModified',function  (event) {
+			anchors = document.querySelectorAll('#cse-body a.gs-title'); //for cse results
+			j = anchors.length;
+			for (; i < j; ++i) {
+				console.log(i);
+				anchors[i].setAttribute('data-cturl','');
+				anchors[i].setAttribute('target','_blank');
+			}
+		},false);
+	} else {
+		anchors = document.querySelectorAll('#ires h3 a'); // google search results' links
+		caches = document.querySelectorAll('#ires .action-menu-panel ul a'); // for webcaches' links
+		for (i = 0,j = anchors.length; i < j; ++i) {
+			anchors[i].onmousedown = null;
+			if (caches[i]) {
+				caches[i].href = caches[i].href.replace('http://','https://');
+				caches[i].onmousedown = null;
+			}
 		}
 	}
 }
