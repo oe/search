1c5b8aaa3aea263fbf4658f50e6f3d775b4a4a67
Saiya <okarl@outlook.com>
2020-10-29 22:45:01 +0800

    feat: update mirrors

--- src/search/mirrors.ts
+++ src/search/mirrors.ts
@@ -1,13 +1,26 @@
 const mirrors = {
   google: [
-    'http://www.google.com'
+    'http://www.google.com',
+    'https://goo.gle.workers.dev',
+    'https://chacha.design',
+    'http://g.histsci.org',
+    'https://gogoo.ml',
+    'https://www.lovec.ltd'
+
   ],
   wiki: [
-    'https://www.wikipedia.org'
+    'https://www.wikipedia.org',
+    'https://www.wikipedia.iwiki.eu.org'
   ],
   scholar: [
-    'https://scholar.google.com'
+    'https://scholar.google.com',
+    'http://so.hiqq.com.cn',
+    'https://sc.panda321.com',
+    'https://xueshu.lanfanshu.cn',
+    'https://xueshu.soogle.top'
   ]
 }
+
+console.log('Google mirrors form Universities: https://www.library.ac.cn/')
 
 export default mirrors
