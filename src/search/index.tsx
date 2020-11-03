77479b7275a138148ded93fd05071b4fc14017d6
Saiya <okarl@outlook.com>
2020-11-04 01:35:25 +0800

    fix: update styles

--- src/search/index.tsx
+++ src/search/index.tsx
@@ -7,33 +7,34 @@ const searchCategories = [
   {
     name: 'google',
     help: () => {
-      const [url, setUrl] = useState('')
+      const [url, setUrl] = useState({})
       useEffect(() => {
-        getAvailableMirrorOf('google').then(u => setUrl(u.url))
+        getAvailableMirrorOf('google').then(u => setUrl(u))
       }, [])
-      return <span>using mirror for Google search: {!url ? 'detecting' : <a href={url} target="_blank">{url}</a>}</span>
+      return <span>using mirror for Google search: <SLink {...url} /></span>
     }
   },
   {
     name: 'wiki',
     help: () => {
-      const [url, setUrl] = useState('')
+      const [url, setUrl] = useState({})
       useEffect(() => {
-        getAvailableMirrorOf('wiki').then(u => setUrl(u.url))
+        getAvailableMirrorOf('wiki').then(u => setUrl(u))
       }, [])
-      return <span>using mirror for Wiki search: {!url ? 'detecting' : <a href={url} target="_blank">{url}</a>}</span>
+      return <span>using mirror for Wiki search: <SLink {...url} /></span>
     }
   },
   {
     name: 'scholar',
     help: () => {
-      const [url, setUrl] = useState('')
-      const [sci, setSci] = useState('')
+      const [url, setUrl] = useState({})
+      const [sci, setSci] = useState({})
       useEffect(() => {
-        getAvailableMirrorOf('scholar').then(u => setUrl(u.url))
-        getAvailableMirrorOf('sci-hub').then(u => setSci(u.url))
+        getAvailableMirrorOf('scholar').then(u => setUrl(u))
+        getAvailableMirrorOf('sci-hub').then(u => setSci(u))
       }, [])
-      return (<span>using mirror for Scholar search: {!url ? 'detecting' : <a href={url} target="_blank">{url}</a>} {sci ? <>, or you can try <a href={sci} target="_blank">sci-hub</a> instead</> : '' }</span>)
+      // @ts-ignore
+      return (<span>using mirror for Scholar search: <SLink {...url} /> {sci.url ? <>, or you can try <SLink {...sci} title="sci-hub" /> instead</> : '' }</span>)
     }
   }
 ]
@@ -62,9 +63,9 @@ export default function SearchInput () {
 }
 
 
-// function SLink(props: IMirrorResult) {
-
-// }
+function SLink(props: {isFailed?: boolean, url?: string, title?: string}) {
+return props.url ? <a className={props.isFailed ? 'is-failed' : ''} href={props.url} target="_blank">{props.title || props.url}</a> : <>detecting...</>
+}
 
 
 function HelpInfo (props: {cat: string}) {
