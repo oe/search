77282501dcb6898aab76bdcc6eb9633078fc5ec1
Saiya <okarl@outlook.com>
2021-05-08 19:13:51 +0800

    feat: add background

--- src/background/index.tsx
+++ src/background/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import './style.scss'
 
 export default function Bg() {
   return (
-    <div>
-      
+    <div className="bg" style={{backgroundImage: "url(https://images.unsplash.com/photo-1619117491206-fdf1558d6a52?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80)"}}>
     </div>
   )
 }
