49319268b4d2464b679c85cfb201d5234f55dcae
Saiya <oklark@outlook.com>
2022-02-26 23:43:12 +0800

    feat: add serviceworker cache

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom'
 
 import App from './app'
 
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register(new URL('sw.ts', import.meta.url), { type: 'module' })
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
