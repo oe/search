bf23ab94416c705ccda3894de044b278971281e5
Saiya <oklark@outlook.com>
2024-01-09 01:27:06 +0800

    fix: fix theme color calculate issue

--- src/background/theme-color.ts
+++ src/background/theme-color.ts
@@ -27,8 +27,10 @@ async function getImageAverageColor(imageSrc: string) {
 
   const pixelCount = imagePixelData.length / 4
   const averRgba = {r: 0, g: 0, b: 0, a: 0}
+
+  const calculatedPointCount = Math.min(pixelCount, MAX_POINT_COUNT)
   // step to calculate average color, to avoid too many calculations
-  const step = Math.ceil(pixelCount / MAX_POINT_COUNT)
+  const step = Math.floor(pixelCount / calculatedPointCount)
   for (let i = 0; i < pixelCount; i += step) {
     let idx = i * 4
     averRgba.r += imagePixelData[idx]
@@ -36,10 +38,10 @@ async function getImageAverageColor(imageSrc: string) {
     averRgba.b += imagePixelData[idx + 2]
     averRgba.a += imagePixelData[idx + 3]
   }
-  averRgba.r = Math.floor(averRgba.r / pixelCount)
-  averRgba.g = Math.floor(averRgba.g / pixelCount)
-  averRgba.b = Math.floor(averRgba.b / pixelCount)
-  averRgba.a = Math.floor(averRgba.a / pixelCount)
+  averRgba.r = Math.floor(averRgba.r / calculatedPointCount)
+  averRgba.g = Math.floor(averRgba.g / calculatedPointCount)
+  averRgba.b = Math.floor(averRgba.b / calculatedPointCount)
+  averRgba.a = Math.floor(averRgba.a / calculatedPointCount)
 
   return`rgba(${averRgba.r}, ${averRgba.g}, ${averRgba.b}, ${averRgba.a / 255})`
 }
